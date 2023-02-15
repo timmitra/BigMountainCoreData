32a9a2d6d1545aa63ec24258bbc0399310481261
Tim Mitra <tim@it-guy.com>
2023-02-15 01:03:13 -0500

    Initial Commit

---
+++ BigMountainCoreData/BigMountainCoreDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  BigMountainCoreDataApp.swift
+//  BigMountainCoreData
+//
+//  Created by Tim Mitra on 2023-02-15.
+//
+
+import SwiftUI
+
+@main
+struct BigMountainCoreDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
