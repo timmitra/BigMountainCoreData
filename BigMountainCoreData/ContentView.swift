7892d4ad0792dbc255d33257542ecaead3c0cacd
Tim Mitra <tim@it-guy.com>
2023-03-11 01:38:10 -0500

    create the ability to add person

--- BigMountainCoreData/ContentView.swift
+++ BigMountainCoreData/ContentView.swift
@@ -11,11 +11,17 @@ struct ContentView: View {
   
   @FetchRequest(sortDescriptors: []) private var people: FetchedResults<PersonEntity>
   // Core Data automatically makes your entities conform to Identifiable
+  @Environment(\.managedObjectContext) var moc
   
     var body: some View {
         VStack {
           List(people) { person in
             Text(person.name ?? "")
+          }
+Button("Add Person") {
+            let person = PersonEntity(context:  moc)
+            person.name = ["Mark", "Lem", "Chase"].randomElement()
+            try? moc.save()
           }
         }
         .font(.title)
