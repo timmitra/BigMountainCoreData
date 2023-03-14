d0034e4eaa2ce69f8647bd355d365ab0aa1be7bd
Tim Mitra <tim@it-guy.com>
2023-03-14 00:31:11 -0400

    set up book data container

--- BigMountainCoreData/BigMountainCoreDataApp.swift
+++ BigMountainCoreData/BigMountainCoreDataApp.swift
@@ -12,7 +12,7 @@ struct BigMountainCoreDataApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
-            .environment(\.managedObjectContext, FirstExampleContainer().persistentContainer.viewContext)
+            .environment(\.managedObjectContext, BooksDataContainer().persistentContainer.viewContext)
         }
     }
 }
