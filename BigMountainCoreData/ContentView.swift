e3bd87d7b45e267f87aea407062bdf6664e5c05e
Tim Mitra <tim@it-guy.com>
2023-05-16 01:18:30 -0400

    sort by multiple factors

--- BigMountainCoreData/ContentView.swift
+++ BigMountainCoreData/ContentView.swift
@@ -10,7 +10,10 @@ import SwiftUI
 struct ContentView: View {
   
   // shorter version of FetchedResults
-    @FetchRequest<ParkEntity>(sortDescriptors: [SortDescriptor(\ParkEntity.name)]) private var parks
+    @FetchRequest<ParkEntity>(sortDescriptors: [
+        SortDescriptor(\.country),
+        SortDescriptor(\.rating)
+    ]) private var parks
   // Core Data automatically makes your entities conform to Identifiable
   
     var body: some View {
