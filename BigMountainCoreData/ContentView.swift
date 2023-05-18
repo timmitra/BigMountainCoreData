b4e3659ec6a0d4cb21e8f66bea74e4525e12881a
Tim Mitra <tim@it-guy.com>
2023-05-17 22:51:52 -0400

    sectionedFetchRequest

--- BigMountainCoreData/ContentView.swift
+++ BigMountainCoreData/ContentView.swift
@@ -23,39 +23,38 @@ extension ParkEntity {
 struct ContentView: View {
     
     @State private var searchText = ""
+    
+    @SectionedFetchRequest<String?, ParkEntity>(sectionIdentifier: \.country, sortDescriptors: [SortDescriptor(\.country)])
   
-  // shorter version of FetchedResults
-    @FetchRequest(fetchRequest: ParkEntity.firstFive)
     private var parks
   // Core Data automatically makes your entities conform to Identifiable
   
     var body: some View {
         NavigationStack {
-            List(parks) { park in
-                HStack {
-                    Image(uiImage: park.viewImage)
-                        .resizable()
-                        .scaledToFit()
-                        .frame(height: 60)
-                        .cornerRadius(8)
-                    
-                    VStack(alignment: .leading, spacing: 4){
-                        Text(park.viewName)
-                            .font(.title)
-                        Text(park.viewLocation)
-                            .fontWeight(.light)
+            List(parks) { section in
+                Section {
+                    ForEach(section) { park in
+                        HStack {
+                            Image(uiImage: park.viewImage)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: 60)
+                                .cornerRadius(8)
+                            
+                            VStack(alignment: .leading, spacing: 4){
+                                Text(park.viewName)
+                                    .font(.title)
+                                Text(park.viewLocation)
+                                    .fontWeight(.light)
+                            }
+                        }
+                        .padding(.vertical, 8)
                     }
-                    
-                    Spacer()
-                }
-                .padding(.vertical, 8)
-            }
-            .safeAreaInset(edge: .bottom) {
-                VStack {
-                    Divider()
-                    Text("Total Parks: \(parks.count)")
+                } header: {
+                    Text(section.id!)
                 }
             }
+            .headerProminence(.increased)
             .navigationTitle("Parks")
        }
     }
