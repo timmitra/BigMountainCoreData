be8dcc1a61d38c1790333636000f2f8d7e6bd44f
Tim Mitra <tim@it-guy.com>
2023-05-26 01:53:21 -0400

    add MOC_Delete

---
+++ BigMountainCoreData/MOC_Delete.swift
@@ -0,0 +1,41 @@
+//
+//  MOC_Delete.swift
+//  BigMountainCoreData
+//
+//  Created by Tim Mitra on 2023-05-26.
+//
+
+import SwiftUI
+import CoreData
+
+struct MOC_Delete: View {
+    
+    @Environment(\.managedObjectContext) var moc
+    @FetchRequest<TaskEntity>(sortDescriptors: [SortDescriptor(\.done)])
+    private var tasks
+    
+    var body: some View {
+        List {
+            ForEach(tasks) { task in
+                Text(task.viewTaskName)
+                    .strikethrough(task.done, color: .red)
+            }
+            .onDelete(perform: deleteTask)
+        }
+    }
+    
+    private func deleteTask(offsets: IndexSet) {
+        for offset in offsets {
+            moc.delete(tasks[offset])
+        }
+        try? moc.save()
+    }
+}
+
+struct MOC_Delete_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .environment(\.managedObjectContext, TasksDataContainer(forPreview: true).persistentContainer.viewContext)
+    }
+}
+
