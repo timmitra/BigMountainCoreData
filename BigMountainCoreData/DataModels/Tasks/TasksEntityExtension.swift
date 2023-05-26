724651bc624c4246dc815354ddc1dcae2e31c958
Tim Mitra <tim@it-guy.com>
2023-05-26 01:17:54 -0400

    set up nils and formatting

--- BigMountainCoreData/DataModels/Tasks/TasksEntityExtension.swift
+++ BigMountainCoreData/DataModels/Tasks/TasksEntityExtension.swift
@@ -5,8 +5,32 @@
 //  Created by Tim Mitra on 2023-05-26.
 //
 
-import Foundation
+import CoreData
+import SwiftUI
 
+// Handle nils and formatting
 extension TasksEntity {
+    var viewTaskName: String {
+        taskName ?? "[Enter Task Name]"
+    }
     
+    var viewDueDate: String {
+        dueDate?.formatted(date: .numeric, time: .omitted) ?? ""
+    }
+    
+    var viewPriority: String {
+        "\(priority).circle.fill"
+    }
+    
+    var viewPriorityColor: Color {
+        if done {
+            return .gray
+        } else if priority == 1 {
+            return .green
+        } else if priority == 2 {
+            return .yellow
+        }
+        
+        return .red
+    }
 }
